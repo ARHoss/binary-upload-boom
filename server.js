6f46a2a6417c2b6c0cb5e82ba33df48e6d4db64e
arhoss <rhossaincse@gmail.com>
2023-03-11 10:50:52 -0700

    added comments

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
+// Getting Express module
 const express = require("express");
+// Express app varibale
 const app = express();
+// Interacts with database & create models
 const mongoose = require("mongoose");
+// Handles authorization - contains strategies for authentication - local strategy
 const passport = require("passport");
+// Handles cookies and sessions
 const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 //Use forms for put / delete
 const methodOverride = require("method-override");
+// Helps in creating flash notification for login
 const flash = require("express-flash");
+// Logger for the application - tells what requests are being made
 const logger = require("morgan");
+// Used to connect to Database
 const connectDB = require("./config/database");
+// Linking to routes
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
 const commentRoutes = require("./routes/comments");
 
-//Use .env file in config folder
+// Environment variables
 require("dotenv").config({ path: "./config/.env" });
 
 // Passport config
@@ -22,13 +31,13 @@ require("./config/passport")(passport);
 //Connect To Database
 connectDB();
 
-//Using EJS for views
+// Tells Express that EJS Template Engine is used
 app.set("view engine", "ejs");
 
-//Static Folder
+// Tells Express to make public folder accessible to public
 app.use(express.static("public"));
 
-//Body Parsing
+// Parses data for express to read request object
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -40,21 +49,24 @@ if(process.env.NODE_ENV === 'development'){
 //Use forms for put / delete
 app.use(methodOverride("_method"));
 
-// Setup Sessions - stored in MongoDB
+// Saves session into database
 app.use(
   session({
+    // Makes it harder to for cookies to reverse-engineer
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    // Storing session info in database
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
 );
 
-// Passport middleware
+// Passport handling authentication
 app.use(passport.initialize());
+// Passport handling sessions
 app.use(passport.session());
 
-//Use flash messages for errors, info, ect...
+// Helps in creating flash messages in front-end
 app.use(flash());
 
 //Setup Routes For Which The Server Is Listening
